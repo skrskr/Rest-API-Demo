0153c17c0a83127735fc3115c1f83cdc279def17
skrskr <mohamed1.sakr1996@gmail.com>
2019-12-24 22:54:55 +0200

    update db mongodb url

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const ordersRouter = require("./api/v1/orders_route");
 const userRouter = require("./api/v1/user_route");
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URL = process.env.MONGODB_URL;
+const MONGO_URL =
+  process.env.MONGODB_URL ||
+  "mongodb://sakr:sakr@cluster0-shard-00-00-ocmui.mongodb.net:27017,cluster0-shard-00-01-ocmui.mongodb.net:27017,cluster0-shard-00-02-ocmui.mongodb.net:27017/apidb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 //Setup Mongodb
 
